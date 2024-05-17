941d8ff32664949820256c35b81f46e3fe098d0d
YinHsun1103 <162529915+YinHsun1103@users.noreply.github.com>
2024-05-17 22:33:12 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -377,18 +377,19 @@ with col3:
    st.image("1a0ec01465964e9fa986689864e47f3d_th.jpg.crdownload")
 
 
-tab1, tab2, tab3 = st.tabs(["阿拉斯加", "邊境牧羊犬", "柴犬"])
+import streamlit as st
+
+tab1, tab2, tab3 = st.columns([1, 1, 1])
 
 with tab1:
-   st.header("阿拉斯加")
-   st.image("S__24829963.jpg")
+    st.header("阿拉斯加")
+    st.image("S__24829963.jpg")
+
 with tab2:
-  st.header("邊境牧羊犬")
-   st.image("images.jpg")
+    st.header("邊境牧羊犬")
+    st.image("images.jpg")
 
 with tab3:
-  st.header("柴犬")
-   st.image("1a0ec01465964e9fa986689864e47f3d_th.jpg.crdownload")
+    st.header("柴犬")
+    st.image("1a0ec01465964e9fa986689864e47f3d_th.jpg.crdownload")
 
-
-, width=200
