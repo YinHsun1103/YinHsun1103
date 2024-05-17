e40c98d3c6f24f539ee9c82c5843b99c3ac70447
YinHsun1103 <162529915+YinHsun1103@users.noreply.github.com>
2024-05-17 22:14:10 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -302,5 +302,5 @@ if st.button("確認"):
 
 
 
-number = st.number_input('輸入一個數字', value=None, placeholder='請在這裡輸入...')
+number = st.number_input('輸入一個數字', value=None, placeholder='請在這裡輸入...',min_value=0,max_value=100)
 st.write('您輸入的是:', number)
