0c1c5ed6dfaac9d4b9a172e8b9b48aabf7a43d47
YinHsun1103 <162529915+YinHsun1103@users.noreply.github.com>
2024-05-17 21:17:17 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,71 +2,66 @@ import streamlit as st
 import random
 import pandas as pd
 import numpy as np
-st.title('Hello,streamlit！我的第一支web應用程式開發!!!')
+from PIL import Image
+
+# Page title and initial text
+st.title('Hello, streamlit！我的第一支web應用程式開發!!!')
 st.write('我是一個字串')
 K = 9999
 st.write(K)
 
-dataframe = np.random.randn(10,20)
+# Displaying a dataframe with random numbers
+dataframe = np.random.randn(10, 20)
 st.write(dataframe)
 
-
-#網頁標題
+# Markdown examples
 st.markdown("# 這是一個示例 Streamlit 網頁")
 st.markdown('''
     :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
     :gray[pretty] :rainbow[colors].''')
-st.markdown("Here's a bouquet &mdash;\
-            #:tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")    
+st.markdown("Here's a bouquet &mdash; #:tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")    
 
-#主標題
+# Titles, headers, and subheaders
 st.title("歡迎來到我的網站")
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
-
-#標頭
 st.header("這是一個標頭", divider='rainbow')
 st.header('_Streamlit_ is :blue[cool] :sunglasses:')
-
-#子標頭
 st.subheader("這是一個子標頭", divider='rainbow')
 st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
 
-#圖片說明
+# Captions
 st.caption("這是一張美麗的圖片")
 st.caption('A caption with italics :blue[colors] and emojis :sunglasses:')
 
-#顯示程式碼, language參考 https://github.com/react-syntax-highl...
+# Displaying code
 st.code("print('Hello, Streamlit!')")
-
 code = '''def hello():
     print("Hello, Streamlit!")'''
 st.code(code, line_numbers=True)    
-
 st.code(code, language='python')
 
-#文字
+# Plain text
 st.text("這是一些純文字內容。")
 
-#數學 LaTeX 表達式
+# LaTeX expressions
 st.latex(r"e^{i\pi} + 1 = 0")
-
 st.latex(r'''
     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
     \sum_{k=0}^{n-1} ar^k =
     a \left(\frac{1-r^{n}}{1-r}\right)
     ''')
 
-#分隔線
+# Dividers
 st.divider()
 st.write("慘了，我被夾在分隔線中間！")
 st.divider()
 
-#---------------------------------------------------------------------------------------------------------------------------------------
-
+# Displaying a styled dataframe
 df = pd.DataFrame(np.random.randn(10, 10), columns=("col %d" % i for i in range(10)))
 st.dataframe(df)  # Same as st.write(df)
 st.dataframe(df.style.highlight_max(axis=0))
 
+# Dataframe with specific columns
 df = pd.DataFrame(
     {
         "name": ["Roadmap", "Extras", "Issues"],
@@ -75,27 +70,9 @@ df = pd.DataFrame(
         "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
     }
 )
-st.dataframe(
-    df,
-    column_config={
-        "name": "App name",
-        "stars": st.column_config.NumberColumn(
-            "Github Stars",
-            help="Number of stars on GitHub",
-            format="%d ⭐",
-        ),
-        "url": st.column_config.LinkColumn("App URL"),
-        "views_history": st.column_config.LineChartColumn(
-            "Views (past 30 days)", y_min=0, y_max=5000
-        ),
-    },
-    hide_index=True
-),
+st.write(df)  # Simplified to avoid issues with column_config
 
-use_container_width=True
-
-#---------------------------------------------------------------------------------------------------------------------------------------
-
+# Displaying tables
 df = pd.DataFrame(np.random.randn(5, 10), columns=("col %d" % i for i in range(10)))
 st.dataframe(df)  # Same as st.write(df)
 st.table(df)  # Same as st.write(df)
