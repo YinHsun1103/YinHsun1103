39d55a4ea28b1d6560d970d96b58f24da34dcfeb
YinHsun1103 <162529915+YinHsun1103@users.noreply.github.com>
2024-11-15 13:02:40 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -565,7 +565,7 @@ elif st.session_state.selected_tab == "HomeWork3":
             data = {
                 "model": "gpt-3.5-turbo",
                 "messages": st.session_state["messages"],
-                "max_tokens": 2000
+                "max_tokens": 4096
             }
 
             try:
